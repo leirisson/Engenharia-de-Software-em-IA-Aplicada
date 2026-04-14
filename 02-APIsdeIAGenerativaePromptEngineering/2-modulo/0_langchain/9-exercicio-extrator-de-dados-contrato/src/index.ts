2819bbb0a8003111e32bb5c45c72c3823a9430ba
leirisson <103783610+leirisson@users.noreply.github.com>
2026-04-13 22:16:56 -0400

    Refactor code structure for improved readability and maintainability

--- 02-APIsdeIAGenerativaePromptEngineering/2-modulo/0_langchain/9-exercicio-extrator-de-dados-contrato/src/index.ts
+++ 02-APIsdeIAGenerativaePromptEngineering/2-modulo/0_langchain/9-exercicio-extrator-de-dados-contrato/src/index.ts
@@ -1,24 +1,38 @@
-import { ChatOpenAI } from '@langchain/openai'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
-import {StringOutputParser} from '@langchain/core/output_parsers'
+import { ChatOpenAI } from "@langchain/openai"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import { JsonOutputParser } from '@langchain/core/output_parsers'
 import { CONFIG } from './config'
 
-const llmModel = new  ChatOpenAI({model: CONFIG.model})
+
+const model = new ChatOpenAI({model: CONFIG.model})
+const parse = new JsonOutputParser()
 
 const prompt = ChatPromptTemplate.fromMessages([
-    ["system", "Voce e um advogado especialista em cobranca. Escreva uma notificacao extrajudicial formal." ],
-    ['human', " com os dados: {devedor}, {valor}, {vencimento}, {credor}"]
+    ["system", `
+        você é um assitente expecialista em extrator de dados de contratos, você
+        deve ler os dados do contrados e a saida esperada é:
+        {{
+        contratante:"",
+        contratado:"",
+        objetivo:"",
+        valor:0,
+        inicio:"",
+        vigencia_meses: ""
+        }}
+
+        `],
+        ["human", "{texto}"]
 ])
 
-const parser = new StringOutputParser();
-
-const chain =  prompt.pipe(llmModel).pipe(parser)
-
-const result = await chain.invoke({
-    devedor: "Maria do carmo",
-    valor: 4500,
-    vencimento: "20/05/2026",
-    credor: "Dr.Marcos da silva"
+const chain = prompt.pipe(model).pipe(parse)
+const contrato = await chain.invoke({
+    texto: `
+    PARTES: João Silva contrata Maria Oliveira para execução do seguinte objeto: desenvolvimento de um site institucional.
+    VALOR E PAGAMENTO: O valor total acordado é de R$ 5.000,00, a ser pago conforme combinado entre as partes.
+    PRAZO: O contrato inicia em 01/05/2026 e terá vigência de 6 meses.
+    OBRIGAÇÕES: Maria Oliveira se compromete a executar o objeto com qualidade e João Silva a efetuar os pagamentos no prazo.
+    RESCISÃO: O descumprimento de qualquer cláusula autoriza a rescisão antecipada, sem prejuízo de perdas e danos.
+`
 })
 
-console.log(result)
+console.log(contrato)
